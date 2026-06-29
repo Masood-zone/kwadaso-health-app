d5c4bdd4c541de4ad212f82fd3117ddbacec8785
Masood <masoodacheampong@gmail.com>
2026-06-29 00:13:49 +0000

    auth-client fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react"
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const authClient = createAuthClient({
-  baseURL: BASE_URL || "http://localhost:3000/api",
+  baseURL: `${BASE_URL}/auth` || "http://localhost:3000/api/auth",
 })
 
 export const { signIn, signOut, signUp, useSession } = authClient
